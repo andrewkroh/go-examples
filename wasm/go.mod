7fac4c47fc0e3486c7a782d6105248ba43dcab82
Andrew Kroh <andrew.kroh@elastic.co>
2021-08-19 18:22:39 -0400

    Switch to go-wasmer as WebAssembly runtime for Go

--- wasm/go.mod
+++ wasm/go.mod
@@ -1,10 +1,5 @@
 module github.com/andrewkroh/go-examples/wasm
 
-go 1.13
+go 1.16
 
-require (
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/perlin-network/life v0.0.0-20191203030451-05c0e0f7eaea
-	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-)
+require github.com/wasmerio/wasmer-go v1.0.4
--- wasm/wasm.go
+++ wasm/wasm.go
@@ -4,31 +4,58 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"github.com/perlin-network/life/exec"
+	"github.com/wasmerio/wasmer-go/wasmer"
 )
 
 func main() {
-	wasm, err := ioutil.ReadFile("sample.wasm")
+	wasmBytes, err := ioutil.ReadFile("sample.wasm")
 	if err != nil {
 		panic(err)
 	}
 
-	vm, err := exec.NewVirtualMachine(wasm, exec.VMConfig{}, &exec.NopResolver{}, nil)
+	// Create an Engine
+	engine := wasmer.NewEngine()
+
+	// Create a Store
+	store := wasmer.NewStore(engine)
+
+	fmt.Println("Compiling module...")
+	module, err := wasmer.NewModule(store, wasmBytes)
+
 	if err != nil {
-		panic(err)
+		fmt.Println("Failed to compile module:", err)
 	}
 
-	entryID, ok := vm.GetFunctionExport("_start") // can be changed to your own exported function
-	if !ok {
-		panic("_start entry function not found")
-	}
+	// Create an empty import object.
+	importObject := wasmer.NewImportObject()
 
-	fmt.Println("Globals: ", vm.Module.Base.Export.Names)
+	fmt.Println("Instantiating module...")
+	// Let's instantiate the Wasm module.
+	instance, err := wasmer.NewInstance(module, importObject)
 
-	ret, err := vm.Run(entryID)
 	if err != nil {
-		vm.PrintStackTrace()
-		panic(err)
+		panic(fmt.Sprintln("Failed to instantiate the module:", err))
 	}
-	fmt.Printf("return value = %d\n", ret)
+
+	// We now have an instance ready to be used.
+	//
+	// From an `Instance` we can fetch any exported entities from the Wasm module.
+	// Each of these entities is covered in others examples.
+	//
+	// Here we are fetching an exported function. We won't go into details here
+	// as the main focus of this example is to show how to create an instance out
+	// of a Wasm module and have basic interactions with it.
+	startFunc, err := instance.Exports.GetFunction("_start")
+
+	if err != nil {
+		panic(fmt.Sprintln("Failed to get the `_start` function:", err))
+	}
+
+	fmt.Println("Calling `_start` function...")
+	result, err := startFunc()
+	if err != nil {
+		panic(fmt.Sprintln("Failed to call the `_start` function:", err))
+	}
+
+	fmt.Println("Results of `_start_start`:", result)
 }
