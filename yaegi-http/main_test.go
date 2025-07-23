6613f8253ea62bcc1f038fb80f0aa2f0f5544bdc
Andrew Kroh <andrew.kroh@elastic.co>
2025-07-23 09:16:42 -0400

    mark copied code

--- yaegi-http/main_test.go
+++ yaegi-http/main_test.go
@@ -40,6 +40,23 @@ func TestScripts(t *testing.T) {
 	testscript.Run(t, p)
 }
 
+// The serve and server functions are adapted from
+// https://github.com/elastic/mito/blob/dev/mito_test.go
+//
+// Copyright 2020 Elasticsearch B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 func serve(ts *testscript.TestScript, neg bool, args []string) {
 	server(ts, neg, "serve", httptest.NewServer, args)
 }
